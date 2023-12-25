b962350f6f4b8fc0c2206dc9599076eb470212cd
YeemiRouth <yeemirouth@gmail.com>
2023-12-25 15:02:50 +1100

    removed some bad modules & listed todo

--- CubyzPlus/Module/ModuleManager.h
+++ CubyzPlus/Module/ModuleManager.h
@@ -1,7 +1,7 @@
 #pragma once
 
-#include "Modules/InfAmmo.h"
-#include "Modules/ImShort.h"
+//#include "Modules/InfAmmo.h"
+//#include "Modules/ImShort.h"
 #include "Modules/Speed.h"
 #include "Modules/Flight.h"
 #include "Modules/Tracers.h"
@@ -9,18 +9,41 @@
 #include "Modules/Aimbot.h"
 #include "Modules/TabUI.h"
 
-void InitMods() {
+void InitMods() { // the commented TODO modules are most likely confirmed modules im gonna be working on
+	// combat
 	modules.push_back(new Aimbot());
+	// TODO: Hitbox - increase a fake hitbox around each player for ghost cheaters
+	// TODO: Tpaura - teleort around the players (or smoothly interpolate) while shooting
 
+	// visual
 	modules.push_back(new TabUI());
 	modules.push_back(new Tracers());
 	modules.push_back(new ESP());
-	modules.push_back(new ImShort());
+	// TODO: Animations - custom animations & other tweaks to interpolated game mechs
+	// TODO: NoHurtcam - disable all hurt effects (or increase them)
 
+	// misc
+	// TODO: ClickTP - teleport to where you right click when holding CTRL
+	// TODO: NetSkip - similar to pingspoof
+	// TODO: Crasher - I realized theres quite a few ways to crash sauerbraten servers with game defined packets
+
+	// movement
 	modules.push_back(new Flight());
 	modules.push_back(new Speed());
+	// TODO: AntiVoid - falling into death pits while hacking hard sucks
+	// TODO: FastSpot - instantly stop moving when letting go of any keys (velocity, ect)
 
-	modules.push_back(new InfAmmo());
+	// player
+	// TODO: AirJump - jump on air
+	// TODO: Spider - climb walls
+	// TODO: Velocity - nothing modifies ur velocity values other then ur movement keys
+	// TODO: Phase - phase through walls
+	// TODO: NoClip - floors too
+	// TODO: FreeCam - allow the camera to fly freely
+
+	// world
+	// TODO: AirStuck - forcefully stick yourself to a single place
+	// TODO: SoundPlayer - allow spam of some sounds
 
 	for (Module* mod : modules) {
 		bool has = false;
