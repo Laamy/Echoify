c47f00ac1cd433731b22eae36cf05e800a32d616
YeemiRouth <yeemirouth@gmail.com>
2023-12-25 15:15:58 +1100

    large list of features to add

--- CubyzPlus/Module/ModuleManager.h
+++ CubyzPlus/Module/ModuleManager.h
@@ -19,19 +19,27 @@ void InitMods() { // the commented TODO modules are most likely confirmed module
 	modules.push_back(new TabUI());
 	modules.push_back(new Tracers());
 	modules.push_back(new ESP());
+	// TODO: ItemESP - esp items idk what else u thought
 	// TODO: Animations - custom animations & other tweaks to interpolated game mechs
 	// TODO: NoHurtcam - disable all hurt effects (or increase them)
+	// TODO: FreeLook - allow looking around freely from a third person perspective whole holding ALT (possibly?)
+	// TODO: NoSway - disable hand swaying
 
 	// misc
 	// TODO: ClickTP - teleport to where you right click when holding CTRL
 	// TODO: NetSkip - similar to pingspoof
 	// TODO: Crasher - I realized theres quite a few ways to crash sauerbraten servers with game defined packets
+	// TODO: AntiBot - allow control over what ESP, aimbot, tracers, ect can hook onto
+	// TODO: NoLagBack - prevents the server from changing your server side position freely
+	// TODO: AntiImmobile - prevents the server from locking your position (so you can move after games end, ect)
+	// TODO: StreamerMode - Change your username/ms
 
 	// movement
 	modules.push_back(new Flight());
 	modules.push_back(new Speed());
 	// TODO: AntiVoid - falling into death pits while hacking hard sucks
 	// TODO: FastSpot - instantly stop moving when letting go of any keys (velocity, ect)
+	// TODO: HighJump - allow controlling of the jump height
 
 	// player
 	// TODO: AirJump - jump on air
@@ -44,6 +52,13 @@ void InitMods() { // the commented TODO modules are most likely confirmed module
 	// world
 	// TODO: AirStuck - forcefully stick yourself to a single place
 	// TODO: SoundPlayer - allow spam of some sounds
+	// TODO: InPvPTower - named after a CTF exploit where you could get millions of CTF points to get onto the server leaderboard in minutes (it works here)
+
+	// other (not modules)
+	// scripting api to allow direct access via javascript or luau(most likely a roblox luau implemenetation as thats what i do most)
+	// custom chat commands
+	// more speed options (for bunny hop)
+	// slash friends to filter out friends from aimbot & aura cheats
 
 	for (Module* mod : modules) {
 		bool has = false;
