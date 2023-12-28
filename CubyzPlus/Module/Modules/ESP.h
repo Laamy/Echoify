bda7f80eeb234b4b804c76a54028e6b49d748afd
YeemiRouth <yeemirouth@gmail.com>
2023-12-28 22:49:46 +1100

    attempt at esp resize stuff

--- CubyzPlus/Module/Modules/ESP.h
+++ CubyzPlus/Module/Modules/ESP.h
@@ -5,49 +5,41 @@ public:
 	ESP() : Module::Module("ESP", "Visual", "Draw boxes with info around players", "yeemi#0", "N") {};
 
 	void OnUpdate(ImDrawData* context) override {
+        if (Game::GetLocalPlayer() == nullptr)
+            return;
 
 		ImVec2 displaySize = ImGui::GetIO().DisplaySize;
 
 		for (fpsent* plyr : Game::Players::GetPlaying()) {
-			ImVec2 screenOut;
-			ImVec2 screenOutBottom;
-			if (
-				Game::ViewMatrix::WorldToScreen(
-					vec(
-						plyr->newpos.x - (plyr->radius / 2),
-						plyr->newpos.y - (plyr->radius / 2),
-						plyr->newpos.z + plyr->aboveeye
-					), screenOut, displaySize) &&
-				Game::ViewMatrix::WorldToScreen(
-					vec(
-						plyr->newpos.x + (plyr->radius / 2),
-						plyr->newpos.y + (plyr->radius / 2),
-						plyr->newpos.z - (plyr->eyeheight - plyr->aboveeye)
-					), screenOutBottom, displaySize))
-			{
-				ImDrawList* list = ImGui::GetBackgroundDrawList();
+            ImVec2 screenOutCenter;
+            if (Game::ViewMatrix::WorldToScreen(
+                vec(plyr->newpos), screenOutCenter, displaySize)) {
 
-				ImU32 colour = ImU32(0xFF0000FF);
+                ImDrawList* list = ImGui::GetBackgroundDrawList();
 
-				if (plyr->GetTeam() == Game::GetLocalPlayer()->GetTeam())
-					colour = ImColor(0, 255, 0);
+                ImU32 colour = ImU32(0xFF0000FF);
 
-				list->AddText(NULL, 18, ImVec2(screenOut.x - 8, screenOut.y - 20), colour, plyr->GetName().c_str());
-				list->AddRect(ImVec2(screenOut.x - 8, screenOut.y - 8), ImVec2(screenOutBottom.x, screenOutBottom.y), colour, 1);
-			}
+                if (plyr->GetTeam() == Game::GetLocalPlayer()->GetTeam())
+                    colour = ImColor(0, 255, 0);
+
+                float distance = Game::GetLocalPlayer()->o.distance(plyr->o) / (plyr->eyeheight * 6); // scale distance properly
+
+                const float baseSizeX = 50.0f;
+                const float baseSizeY = baseSizeX * 2.0f;
+                float boxSizeX = max(baseSizeX / distance, 5.0f);
+                float boxSizeY = max(baseSizeY / distance, 10.0f);
+
+                ImVec2 boxMin = ImVec2(screenOutCenter.x - boxSizeX / 2, screenOutCenter.y - boxSizeY / 2);
+                ImVec2 boxMax = ImVec2(screenOutCenter.x + boxSizeX / 2, screenOutCenter.y + boxSizeY / 2);
+
+                // adjust
+                boxMin.y += boxSizeY / 2;
+                boxMax.y += boxSizeY / 2;
+
+                // Draw the box and text
+                list->AddText(NULL, 18, ImVec2(screenOutCenter.x, screenOutCenter.y), colour, plyr->GetName().c_str());
+                list->AddRect(boxMin, boxMax, colour, 1);
+            }
 		}
-
-		// niggers
-		/*for (fpsent* plyr : Game::Players::GetSpectating()) {
-			ImVec2 screenOut;
-			if (Game::ViewMatrix::WorldToScreen(plyr->newpos, screenOut, displaySize)) {
-				ImDrawList* list = ImGui::GetBackgroundDrawList();
-
-				ImU32 colour = ImU32(0xFFFF0000);
-
-				list->AddText(NULL, 18, ImVec2(screenOut.x - 8, screenOut.y - 20), colour, plyr->GetName().c_str());
-				list->AddRect(ImVec2(screenOut.x - 8, screenOut.y - 8), ImVec2(screenOut.x + 8, screenOut.y + 15), colour, 1);
-			}
-		}*/
 	}
 };
